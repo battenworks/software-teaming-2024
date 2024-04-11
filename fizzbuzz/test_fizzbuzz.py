8703564152a6aa9120f937aeb8bd3f3bc26ac1d4
Chris Pipito <cpipito29@gmail.com>
2024-04-10 20:24:00 -0400

    formatting

--- fizzbuzz/test_fizzbuzz.py
+++ fizzbuzz/test_fizzbuzz.py
@@ -1,10 +1,11 @@
 import unittest
 import fizzbuzz
 
+
 class TestFizzBuzz(unittest.TestCase):
     def test_returns_1_for_1(self):
         self.assertEqual("1", fizzbuzz.fizzbuzz(1))
-    
+
 
 if __name__ == '__main__':
     unittest.main()
