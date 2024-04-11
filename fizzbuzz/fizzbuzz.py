760a96e7673021a9869de8d5c65f199b8a6461ea
battenworks <djb@battenworks.com>
2024-04-11 13:10:49 -0600

    Here's my message

---
+++ fizzbuzz/fizzbuzz.py
@@ -0,0 +1,8 @@
+
+def fizzbuzz(number):
+    if number == 3:
+        return "fizz"
+    if number == 5:
+        return "buzz"
+    
+    return str(number)
--- fizzbuzz/test_fizzbuzz.py
+++ fizzbuzz/test_fizzbuzz.py
@@ -6,6 +6,15 @@ class TestFizzBuzz(unittest.TestCase):
     def test_returns_1_for_1(self):
         self.assertEqual("1", fizzbuzz.fizzbuzz(1))
 
+    def test_returns_fizz_for_3(self):
+        self.assertEqual("fizz", fizzbuzz.fizzbuzz(3))
+
+    def test_returns_buzz_for_5(self):
+        self.assertEqual("buzz", fizzbuzz.fizzbuzz(5))
+
+    def test_returns_fizzbuzz_for_15(self):
+        self.assertEqual("fizzbuzz", fizzbuzz.fizzbuzz(15))
+
 
 if __name__ == '__main__':
     unittest.main()
